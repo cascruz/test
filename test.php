36e8e4fc1d0955e6c00ed16efdc12e11445b2764
cascruz <150393248+cascruz@users.noreply.github.com>
2023-11-11 17:30:07 -0600

    Add files via upload
    Replace name "test.php" with any other file you're using.

---
+++ test.php
@@ -0,0 +1,29 @@
+<?php
+	include ('shared.php');
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="keywords" content="HTML Template">
+        <meta name="description" content="Cold Pressed Cafe | HTML Template">
+        <meta name="author" content="Student Name">
+
+        <title>Cold Pressed Cafe</title>
+       <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+
+
+
+	<!-- add a php function call to generate the navigation menu below -->
+<?php echo $nav;
+?>
+
+     <!-- ADD HTML HERE -->
+
+
+        	<?php echo $footer; ?>
+    </body>
+ </html> 
